7f1acb1c5e61bb271656488cf7ea21a5858b8c49
Mehran Moazeni <mehran1414@gmail.com>
2025-05-12 21:27:55 +0200

    pull req updated

--- src/game_sales/data_process.py
+++ src/game_sales/data_process.py
@@ -3,7 +3,7 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import current_timestamp, to_utc_timestamp
 from sklearn.model_selection import train_test_split
 
-from mlops_course.config import ProjectConfig
+from game_sales.config import ProjectConfig
 
 
 class DataProcessor:
